f852be880a6c90de21fc9102dc4386341a1ad379
adamaxy <adam996@libero.it>
2023-07-19 13:33:13 +0200

    aggiunta esercizio if else

---
+++ main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        String myname = "giovanni";
+        if (myname.length() % 2 == 0){
+            System.out.println("il numero di lettere nel mio nome è pari");
+        }
+        else {System.out.println("il numero di lettere nel mio nome è dispari");
+
+        }
+    }
+}
